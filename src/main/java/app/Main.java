d9926d9e04d6afe1fe6652b14c21ef13cf4ca03e
pcadmin <mohamadahmadpour1383@gmail.com>
2023-07-11 17:36:53 +0330

    move to config

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -35,8 +35,8 @@ public class Main {
 
     private static void connect() throws ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/university",
-                "postgres", "1274335299")) {
+        try (Connection connection = DriverManager.getConnection(Config.getInstance().getUrl(),
+                Config.getInstance().getUsername(), Config.getInstance().getPassword())) {
             System.out.println("connected");
             View.getInstance().run(connection);
         } catch (SQLException e) {
---
+++ src/main/java/model/Config.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Config {
+    private static Config instance;
+
+    private Config() {
+    }
+
+    public static Config getInstance() {
+        if (instance == null)
+            instance = new Config();
+        return instance;
+    }
+
+    public String getUsername() {
+        return "postgres";
+    }
+    public String getPassword() {
+        return "1274335299";
+    }
+    public String getUrl() {
+        return "jdbc:postgresql://localhost:5432/university";
+    }
+}
